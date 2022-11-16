cc05e9fe041b051337be9724addc3c87c17dc011
Daniel Pérez <steew0x8@protonmail.com>
2022-11-16 14:47:42 +0100

    Debug and CLINT test

--- src/main.rs
+++ src/main.rs
@@ -4,45 +4,41 @@
 extern crate panic_halt;
 
 use hifive1::{
-    hal::core::plic::Priority,
     hal::DeviceResources,
     hal::{core::CorePeripherals},
     hal::{e310x::interrupt::Interrupt, prelude::*},
     pin, sprintln,
 };
 use riscv_rt::entry;
-/* use riscv::register::{mstatus, mie}; */
+use riscv::register::{mstatus, mie};
 /* Sample function to toggle a led on interrupt handle.
 For now we will just write to stdout. */
-fn led_interrupt() {
+unsafe fn led_interrupt() {
     sprintln!("INTERRUPTED");
+    let dr = DeviceResources::steal();
+    let gpio = dr.pins;
+    let gpio13 = pin!(gpio, dig13);
+    /* Get system board LED */
+    let mut test_led = gpio13.into_output();
+    test_led.toggle().unwrap();
 }
 
-/* PLIC Interrupt test (Timer interrupt) */
-/* We will export this as MachineExternal as mentioned by the
+/* CLINT Interrupt test (Timer interrupt) */
+/* We will export this as MachineTimer as mentioned by the
 riscv-rt documentation. This function will serve as the external
 interrupts handler written in the mtvec in direct mode.
-We will use it to test RTC timer interrupts with PLIC, but could also
-be done with CLINT's mtimecmp register. */
+We will use it to test mtimecmp interrupts with CLINT 
+*/
 #[no_mangle]
-#[export_name = "MachineExternal"]
-unsafe extern "C" fn MachineExternal() {
-    sprintln!("Received an interrupt");
-    /* Steal the PLIC again, now to claim the interrupt */
-    let mut plic = CorePeripherals::steal().plic;
-    /* Claim the pending interrupt */
-    let intr = plic.claim.claim().unwrap();
-    /* Match the interrupt (we only have one to handle) */
-    match intr {
-        Interrupt::RTC => {
-            led_interrupt();
-        }
-        _ => {
-            sprintln!("We received an interrupt we don't know how to handle");
-        }
-    }
-    /* Complete the interrupt */
-    plic.claim.complete(intr);
+#[export_name = "MachineTimer"]
+unsafe fn MachineTimer() {
+    // Call our handler
+    led_interrupt();
+    /* Steal the CLINT */
+    let mut clint = CorePeripherals::steal().clint;
+    /* rewrite the mtimecmp register to clear the interrupt */
+    clint.mtimecmp.set_mtimecmp(clint.mtime.mtime() + 100000);
+    sprintln!("mtimecmp: {}", clint.mtimecmp.mtimecmp());
 }
 
 /* Main riscv-rt entry point */
@@ -73,36 +69,17 @@ fn main() -> ! {
     // let gpio13 = pin!(gpio, dig13);
     /* Get system board LED */
     // let mut test_led = gpio13.into_output();
-
-    /* RTC CONFIGURATION */
-    /* Get the RTC peripheral to configure it */
-    /* Call the constrain() function to return a copy of the value */
-    let mut rtc = peripherals.RTC.constrain();
     
-    rtc.enable();
-    /* Set default counting scale */
-    rtc.set_scale(0);
-    /* Initialize it to zero */
-    rtc.set_rtc(0);
-    /* Similar to initializing mtimecmp, target value to compare rtc register with */
-    rtc.set_rtccmp(2000);
-    /* Enable our rtc peripheral counter */
-    rtc.enable();
-
-    /* PLIC CONFIGURATION */
+    /* Get the CLINT */
+    let mut clint = resources.core_peripherals.clint;
+    /* Set the mtimecmp register to 1 second, that is 64.000.000 clock cycles    */
+    clint.mtimecmp.set_mtimecmp(clint.mtime.mtime() + 100000);
+    /* CLINT CONFIGURATION */
     unsafe {
-        /* Steal the PLIC controller temporarily to modify (singleton) */
-        let mut plic = CorePeripherals::steal().plic;
-        /* Now working with PLIC's rtc interrupt */
-        /* Set max priority to the rtc interrupt */
-        plic.rtc.set_priority(Priority::P7);
-        /* Enable the rtc interrupt in PLIC */
-        plic.rtc.enable();
-        /* Set the interrupt threshold (P0 = no threshold) */
-        plic.threshold.set(Priority::P1);
-        /* Enable machine external interrupts to allow RTC peripheral
-        to notify us */
-        plic.mext.enable();
+        /* Activate mtime interrupt from the MIE register */
+        mie::set_mtimer();
+        /* Activate global interrupts (mie bit) */
+        mstatus::set_mie();
     }
     sprintln!("Finished configuring everything");
     /* Main system loop (leave empty to test RTC) */
